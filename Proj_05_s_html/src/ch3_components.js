4f408cc03fb688ce2a05bcf54b0449e442a5bc4a
Rody <rody0335@hotmail.com>
2018-03-22 16:37:45 +0800

    Modify Bug js

--- Proj_05_s_html/src/ch3_components.js
+++ Proj_05_s_html/src/ch3_components.js
@@ -3,11 +3,11 @@ $(document).ready(() => {
         $('.alert').alert('close')
     })
 
-    $('alert').on('close.bs.alert', () => {
+    $('.alert').on('close.bs.alert', () => {
         console.log('close alert')
     })
 
-    $('alert').on('closed.bs.alert', () => {
+    $('.alert').on('closed.bs.alert', () => {
         console.log('closed alert')
     })
 })
